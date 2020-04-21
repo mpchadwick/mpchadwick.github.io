8b63f3b8873b422713ac3bd9c8d16e9828f4ee14
Max Chadwick <mpchadwick@gmail.com>
2020-04-20 21:33:58 -0400

    Update twitter script to support strict-dynamic

---
+++ scripts/twitter.js
@@ -0,0 +1,9 @@
+(function() {
+var d = document, s = d.createElement('script');
+
+s.src = '//platform.twitter.com/widgets.js';
+s.async = true;
+s.setAttribute('charset', 'utf-8');
+
+(d.head || d.body).appendChild(s);
+})();

7c023e470dc3995bdb50120eedac6fe0fdd6e6f0
Max Chadwick <mpchadwick@gmail.com>
2020-05-03 20:07:57 -0400

    Send Hit ID

--- scripts/ga.js
+++ scripts/ga.js
@@ -2,6 +2,12 @@
 layout: null
 ---
 
+// https://gist.github.com/jed/982883
+const uuid = function b(a) {
+  return a ? (a ^ Math.random() * 16 >> a / 4).toString(16) :
+      ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, b);
+};
+
 {% if jekyll.environment == 'production' %}
   {% assign ga = site.ga.prod %}
 {% else %}
@@ -16,6 +22,9 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', '{{ ga }}', 'auto');
 
 ga('send', 'pageview');
+
+// Hit ID
+ga('set', 'dimension1', uuid());
 
 var tags = JSON.parse(document.getElementById('ga-tags').textContent);
 
